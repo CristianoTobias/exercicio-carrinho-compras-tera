46c88d06760ea03d274b8f830ecc12ab57fe177b
Cristiano Tobias <cristiano.tobias40@gmail.com>
2023-01-18 18:35:27 -0300

    Adicionado funçao para calculo total com as condiçoes para os descontos

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-let item1 = parseFloat(document.getElementById('item-1').innerHTML)
-let item2 = parseFloat(document.getElementById('item-2').innerHTML)
-let item3 = parseFloat(document.getElementById('item-3').innerHTML)
+let item1 = parseFloat(document.getElementById("item-1").innerHTML);
+let item2 = parseFloat(document.getElementById("item-2").innerHTML);
+let item3 = parseFloat(document.getElementById("item-3").innerHTML);
 
-let  qtdItem1 = document.getElementById('form1')
-let  qtdItem2 = document.getElementById('form2')
-let  qtdItem3 = document.getElementById('form3')
+let qtdItem1 = document.getElementById("form1");
+let qtdItem2 = document.getElementById("form2");
+let qtdItem3 = document.getElementById("form3");
 
-let subTotal =  document.getElementById('subtotal-items')
-let total =  document.getElementById('total-items')
+let shipping = parseFloat(document.getElementById("shipping").innerHTML);
+let tax = parseFloat(document.getElementById("tax").innerHTML);
 
+let payment = document.getElementById('payment')
+
+let subTotal = document.getElementById("subtotal-items");
+let total = document.getElementById("total-items");
 
 qtdItem1.onclick = () => {
-    console.log(subTotal)
-   subTotal.innerHTML = subTotalItems()
-   total.innerHTML = subTotalItems()
-   
-
-}
+  subTotalItems();
+  total.innerHTML = totalItems();
+};
 
 qtdItem2.onclick = () => {
-    subTotal.innerHTML = subTotalItems()
-    total.innerHTML = subTotalItems()
-
-}
+  subTotalItems();
+  total.innerHTML = totalItems();
+};
 
 qtdItem3.onclick = () => {
-    subTotal.innerHTML = subTotalItems()
-    total.innerHTML = subTotalItems()
+  subTotalItems();
+  total.innerHTML = totalItems();
+};
 
+payment.onclick = () => {
+    subTotalItems();
+    total.innerHTML = totalItems();
 }
 
 const subTotalItems = () => {
-   return (item1 * qtdItem1.value) + (item2 * qtdItem2.value) + (item3 * qtdItem3.value)
-   
-}
+  subTotal.innerHTML = (
+    item1 * qtdItem1.value +
+    item2 * qtdItem2.value +
+    item3 * qtdItem3.value
+  ).toFixed(2);
+  return (
+    item1 * qtdItem1.value + item2 * qtdItem2.value + item3 * qtdItem3.value
+  );
+};
+
+const totalItems = () => {
+  let paymentOption = payment.options[payment.selectedIndex].text;
+  switch (paymentOption) {
+    case "Money":
+        return subTotalItems > 200 ? ((subTotalItems() + shipping + tax) * 0.9).toFixed(2): ((subTotalItems() + shipping + tax) * 0.85).toFixed(2)
+      
+      break;
+    case "Pix":
+        return subTotalItems > 200 ? ((subTotalItems() + shipping + tax) * 0.95).toFixed(2): ((subTotalItems() + shipping + tax) * 0.90).toFixed(2)
+      break;
+    default:
+      return (subTotalItems() + shipping + tax).toFixed(2);
+  }
+};
